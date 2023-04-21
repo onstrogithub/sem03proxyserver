644d8c288c556059763728f730863a9a8a3a681c
onstrogithub <onstro18@uia.no>
2023-04-21 09:56:13 +0000

    commit

--- tcp_proxyserver.go
+++ tcp_proxyserver.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"sync"
 )
 
 func main() {
@@ -26,7 +27,34 @@ func main() {
 func handleConnection(client net.Conn) {
 	defer client.Close()
 
-	server, err := net.Dial("tcp","172.20.0.1:6000") //"172.17.0.2:5000")
+	server, err := net.Dial("tcp", "172.17.0.2:5000")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer server.Close()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		io.Copy(server, client)
+		wg.Done()
+	}()
+	go func() {
+		io.Copy(client, server)
+		wg.Done()
+	}()
+	wg.Wait()
+}
+/*
+Dette var koden jeg orginalt sett hadde for handleconnection. Metoden over ble delvis gjennomfort av chatgpt. Jeg fikk laget en forbindelse 
+og samlet pakker med koden nedenfor, men etter rundt 1 minutt saa begynnte jeg aa faa feilmeldinger. 
+Pakkene ble samlet og pushet til github. Har pakkene fra feilmeldingen paa wireshark dersom dette 
+blir interessant aa se paa paa et senere tidspunkt.
+func handleConnection(client net.Conn) {
+	defer client.Close()
+
+	server, err := net.Dial("tcp", "172.20.0.1:6000")
 	if err != nil {
 		log.Println(err)
 		return
@@ -36,4 +64,4 @@ func handleConnection(client net.Conn) {
 	go io.Copy(server, client)
 	io.Copy(client, server)
 }
-
+*/
